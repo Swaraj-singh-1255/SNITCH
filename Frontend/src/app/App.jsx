0f4d587ccfda06210723d8c66b3bc13c991df6f5
Swaraj-singh-1255 <swarajsinghrajput1255@gmail.com>
2026-05-27 23:51:16 +0530

    feat: add CORS support and user authentication endpoints

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
 
 import './App.css'
+import { RouterProvider } from 'react-router'
+import { routes } from './app.routes'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Hello!</h1>
+    <RouterProvider router={routes} />
     </>
   )
 }
